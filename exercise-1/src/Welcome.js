651bc9d6eddaf43bf9134b43ef7709d7a87f45de
antonio l'abbate <antolabb06@gmail.com>
2023-04-27 17:27:00 +0200

    exercise-7

---
+++ exercise-1/src/Welcome.js
@@ -0,0 +1,11 @@
+function Welcome(props){
+    return(
+        <>
+        <h1>Hello World!</h1>
+        <p>Welcome, {props.name}!</p>
+        <p>Your age is,{props.age}!</p>
+        </>
+    );
+}
+
+export default Welcome;
