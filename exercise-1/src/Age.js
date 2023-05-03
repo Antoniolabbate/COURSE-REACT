4b2ea65d58cfb5a4e6ffe83c5134e7bf11c5e837
antonio l'abbate <antolabb06@gmail.com>
2023-05-03 14:58:06 +0200

    exercise 11

--- exercise-1/src/Age.js
+++ exercise-1/src/Age.js
@@ -1,8 +1,8 @@
-
 export default function Age(props) {
-    return (
-      <>
-        <p>Your age is {props.age ? (props.age < 18 ? "less than 18" : props.age) : null}!</p>
-      </>
-    );
-  }
+  let age = props.age;
+  return (
+    <>
+      <p>Your age is {age && age > 18 && age < 65 ? age : "less than 18 or greater than 65"}!</p>
+    </>
+  );
+}
