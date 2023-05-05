ec3159a067b25d5558e5dc79d6093cb928694e9c
antonio l'abbate <antolabb06@gmail.com>
2023-05-05 19:57:29 +0200

    exercise 19

--- exercise-1/src/App.js
+++ exercise-1/src/App.js
@@ -1,14 +1,21 @@
-import {useState} from 'react';
-export default function App(){
-  const [counter, setCounter] = useState(0);
-  const increase = () => {
-    setCounter(count => count + 1);
-  };
+import React from 'react';
+import { useState } from 'react';
+function ClickTracker(){
+  const [lastClicked, setLastClicked] = useState('');
+  function handleClick(e){
+    //registra l'evento di click e l'ultimo pulsante premuto
+    setLastClicked(e.target.name);
+  }
+  const style = {
+    color: "green"
+  }
   return(
-    <>
-    <h1>{counter}</h1>
-    <button onClick={increase}>Increase</button>
-    
-    </>
-  )
-}
+    <div>
+      <h1 style={style}>Ultimo pulsante premuto: {lastClicked}</h1>
+       <button onClick={handleClick} name="button1">Primo pulsante</button>
+      <button onClick={handleClick}name='button2'>Secondo pulsante</button>
+      <button onClick={handleClick}name='button3'>Terzo pulsante</button>
+    </div>
+  );
+  }
+  export default ClickTracker;
