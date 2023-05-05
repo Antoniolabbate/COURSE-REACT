57e22c8f916a53a8d22399a8795056b952eea0df
antonio l'abbate <antolabb06@gmail.com>
2023-05-05 17:42:38 +0200

    exercise 16

--- exercise-1/src/App.js
+++ exercise-1/src/App.js
@@ -1,16 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import { useEffect, useState } from "react";
+
+function CounterDisplay(props) {
+  return <h1>{props.count}</h1>;
+}
 
 function Counter(props) {
-  const [count, setCount] = useState(props.initialCount);
+  const [count, setCount] = useState(props.initialValue);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      setCount(count => count + props.increment);
-    },props.interval);
+      setCount(count => count + props.incrementAmount);
+    }, props.incrementInterval);
     return () => clearInterval(intervalId);
   }, [props]);
 
-  return <h1>{count}</h1>;
+  return (
+    <div>
+      <CounterDisplay count={count} />
+    </div>
+  );
 }
 
 export default Counter;
