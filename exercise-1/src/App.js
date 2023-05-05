8480e5da6c49e19e45b8f0350e89ed9ece302ebf
antonio l'abbate <antolabb06@gmail.com>
2023-05-05 17:18:21 +0200

    exercise 15

--- exercise-1/src/App.js
+++ exercise-1/src/App.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
 
-function Counter() {
-  const [count, setCount] = useState(0);
+function Counter(props) {
+  const [count, setCount] = useState(props.initialCount);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      setCount(count => count + 1);
-    }, 1000);
+      setCount(count => count + props.increment);
+    },props.interval);
     return () => clearInterval(intervalId);
-  }, []);
+  }, [props]);
 
   return <h1>{count}</h1>;
 }
