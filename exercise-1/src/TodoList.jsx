5c4c099371c48efef708c4bd26b59319acfd7715
antonio l'abbate <antolabb06@gmail.com>
2023-05-07 18:39:19 +0200

    exercise 26

--- exercise-1/src/TodoList.jsx
+++ exercise-1/src/TodoList.jsx
@@ -1,32 +1,34 @@
-import { useState } from "react";
-function TodoList() {
-    const [list, setList] = useState([]);
-    const [input, setInput] = useState("");
-    const addTodo = (todo) => {
-        const newTodo = {
-            id: Math.random(),
-            todo: todo,
-        };
-        setList([...list, newTodo])
-        //clear input boxe
-        setInput("")
-    }
-    return(
-        <div>
-            <h1>TodoList</h1>
-            <input type="text"  value={input} onChange={(e) => setInput(e.target.value)}/>
-            <button onClick={() => addTodo(input)}>Add</button>
-            <ul>
-                {list.map((todo) => (
-                    <li key={todo.id}>
-                        {todo.todo}
-                        </li>
-                ))}
-           
-            </ul>
-        </div>
-    )
-}
+import React, { useState } from "react";
 
+function TodoList() {
+  const [list, setList] = useState([]);
+  const [input, setInput] = useState("");
+
+  const addTodo = (todo) => {
+    const newTodo = {
+      id: Math.random(),
+      todo: todo,
+    };
+    setList([...list, newTodo]);
+    setInput(""); // Cancella il contenuto dell'input box dopo l'aggiunta di un nuovo todo
+  };
+
+  const handleInputChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  return (
+    <div>
+      <h1>TodoList</h1>
+      <input type="text" value={input} onChange={handleInputChange} />
+      <button onClick={() => addTodo(input)}>Add</button>
+      <ul>
+        {list.map((todo) => (
+          <li key={todo.id}>{todo.todo}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 export default TodoList;
