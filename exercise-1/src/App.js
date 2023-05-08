d20b243ae6007b4e98ea5f05366d8d1eb0e692bc
antonio l'abbate <antolabb06@gmail.com>
2023-05-09 00:05:56 +0200

    exercise 32

--- exercise-1/src/App.js
+++ exercise-1/src/App.js
@@ -1,14 +1,45 @@
-import {useState} from 'react';
-export default function App(){
-  const [counter, setCounter] = useState(0);
-  const increase = () => {
-    setCounter(count => count + 1);
-  };
+import {useState} from "react"
+
+const Login = () => { 
+  const [username, setUsername] = useState("");
+  const [password, setPassword]  = useState("");
+  const [remember, setRemember] = useState(false);
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  }
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  }
+  const handleRememberChange = (event) => {
+    setRemember(event.target.checked);
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault()
+  }
+  
+
+
   return(
-    <>
-    <h1>{counter}</h1>
-    <button onClick={increase}>Increase</button>
-    
-    </>
+    <div>
+      <label>
+        username:
+      <input type="text"  value={username} onChange={handleUsernameChange}/>
+      </label>
+      <label>
+        password:
+      <input type="password" value={password} onChange={handlePasswordChange}/>
+      </label>
+      <label>
+        remeber me:
+        <input type="checkbox" value={remember} onChange={handleRememberChange}/>
+      </label>
+      <label>
+        <button onClick={handleSubmit}>Login</button>
+      </label>
+
+
+    </div>
   )
 }
+export default Login;
