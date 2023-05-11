8f42775316a404aecb736d00bd69c550c5815fe9
antonio l'abbate <antolabb06@gmail.com>
2023-05-11 23:01:24 +0200

    exercise-30

--- exercise-1/src/App.js
+++ exercise-1/src/App.js
@@ -1,22 +1,31 @@
-import React from "react";
-import TodoList from "./TodoList";
+import React, { useState, createContext } from "react";
+import DisplayLanguage from "./DisplayLanguage";
+
+// Creiamo il contesto
+export const LanguageContext = createContext();
 
 function App() {
+  // Definiamo lo stato per la lingua selezionata
+  const [language, setLanguage] = useState("italiano");
+
+  // Funzione per gestire la selezione della lingua
+  const handleLanguageChange = (event) => {
+    setLanguage(event.target.value);
+  };
+
   return (
-    <div>
-      <TodoList
-        render={({ list, deleteTodo }) => (
-          <ul>
-            {list.map((todo) => (
-              <li key={todo.id}>
-                {todo.todo}
-                <button onClick={() => deleteTodo(todo.id)}>&times;</button>
-              </li>
-            ))}
-          </ul>
-        )}
-      />
-    </div>
+    // Avvolgiamo il componente DisplayLanguage nel provider del contesto
+    <LanguageContext.Provider value={{ language }}>
+      <div>
+        <h1>Language Selector</h1>
+        <select value={language} onChange={handleLanguageChange}>
+          <option value="italiano">Italiano</option>
+          <option value="inglese">Inglese</option>
+          <option value="spagnolo">Spagnolo</option>
+        </select>
+        <DisplayLanguage />
+      </div>
+    </LanguageContext.Provider>
   );
 }
 
