4691b8636e989d5d6c92c847f39c38a0374e64f5
antonio l'abbate <antolabb06@gmail.com>
2023-05-19 17:16:43 +0200

    exercise 40

--- exercise-1/src/GithubUser.jsx
+++ exercise-1/src/GithubUser.jsx
@@ -1,33 +1,21 @@
 import React from "react";
-import { useEffect, useState } from "react";
-import GithubUserList from "./GithubUserList";
+import useGithubUser from "./useGitHubUser";
 
 
-function GithubUser({username}){
-    const [user, setUser] = useState("");
 
-    useEffect(() =>{
-        async function fetchData(){
-            try {
-                const response = await fetch(  `https://api.github.com/users/${username}`)
-                const data = await response.json()
-                setUser(data);
-                
-            } catch (error) {
-                console.log(error);
-                setUser(null)
-                
-            }
-        }
-        fetchData()
-    },[])
-    return(
-        <div>
-            <h2>{user.login ? user.login : 'username error'}</h2>
-            <h3>
-                {user.id ? user.id : 'id error'}, {user.name ? user.name : 'username missing'}
-            </h3>
-        </div>
-    )
+function GithubUser({ username }) {
+  const {data} = useGithubUser(username)
+
+  return(
+    <>
+      {data && data.login}
+    </>
+  )
+  
+    
+  
+
+
 }
-export default GithubUser
+
+export default GithubUser;
