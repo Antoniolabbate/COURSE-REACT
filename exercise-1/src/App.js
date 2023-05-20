9745ac502ce2923817b298fcd83b48fbac4703bb
antonio l'abbate <antolabb06@gmail.com>
2023-05-20 11:19:03 +0200

    exercise-47

--- exercise-1/src/App.js
+++ exercise-1/src/App.js
@@ -1,26 +1,31 @@
-import { Route, Routes, Link} from "react-router-dom"
-import Home from "./pages/Home"
-import Counter from "./pages/Counter"
-
+import { Route, Routes, Link } from "react-router-dom";
+import ShowGithubUser from "/ShowGithubUser";
+import Home from "./Home";
+import Counter from "./Counter";
+import GithubUser from "/GitHubUser";
 
 const App = () => {
-
-
   return (
     <>
-      <nav>
-        <Link style={{marginRight: '20px'}} to={'/'}>Home</Link>
-        <Link to={'/pages/Counter'}>Counter</Link>
+      <nav style={{ display: "flex", gap: "20px" }}>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/Counter"}>Counter</Link>
+        <Link to="/ShowGithubUser/jhon">ShowGithubUser</Link>
+        <Link to={"/GithubUser/Jhon"}>GithubUser</Link>
       </nav>
 
       <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/pages/Counter" element={<Counter/>}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/Counter" element={<Counter />}></Route>
+        <Route
+          path="/ShowGithubUser/:username"
+          element={<ShowGithubUser />}
+        ></Route>
+        <Route path="/GithubUser/:username" element={<GithubUser />}>
+        </Route>
       </Routes>
-
-
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
