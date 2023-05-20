edc176f9ba16c6f4937aa918a512c14d5fd741b1
antonio l'abbate <antolabb06@gmail.com>
2023-05-20 11:03:16 +0200

    exercise-46

--- exercise-1/src/App.js
+++ exercise-1/src/App.js
@@ -1,14 +1,20 @@
-import Counter from "./Counter";
-
+import FilteredList from "./FilteredList";
 
 const App = () => {
 
+  const obj = [
+    { name: 'Luca', id: 1, age: 30 },
+    { name: 'Jhon', id: 2, age: 17 },
+    { name: 'Joe', id: 3, age: 16 },
+    { name: 'Liam', id: 4, age: 42 },
+    { name: 'Seth', id: 5, age: 20 },
+  ];
 
   return (
     <>
-      <Counter/>
+      <FilteredList list={obj}/>
     </>
-  )
-}
+  );
+};
 
 export default App;
