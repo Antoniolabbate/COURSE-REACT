a8390dad2fd25cb684b94957362f9e0bf9d1f5c7
antonio l'abbate <antolabb06@gmail.com>
2023-05-21 16:39:52 +0200

    exercise-53

--- exercise-1/src/App.js
+++ exercise-1/src/App.js
@@ -1,29 +1,10 @@
-import React from "react";
-import { Router, Routes, Route, Link } from "react-router-dom";
-import GithubUserList from "./components/GithubUserList";
-import ShowGithubUser from "./components/ShowGithubUser";
-import Home from "./components/Home";
+import GithubUser from "./components/GithubUser"
+function App(){
 
-function App() {
-  return (
-    <>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/users">Users</Link>
-      </nav>
-
-      <Router>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/users" component={GithubUserList} />
-        <Route path="/users/:username" component={ShowGithubUser} />
-      </Router>
-    </>
-  );
+    return<div>
+            <GithubUser />
+        </div>
+    
 }
 
 export default App;
-
-
-// You cannot render a <Router> inside another <Router>.
-// You should never have more than one in your App.
-// Mi esce questo errore non capisco. 
