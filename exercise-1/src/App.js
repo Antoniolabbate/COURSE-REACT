cdaed1c2a0e513e47c184cd091bc1e32641efc57
antonio l'abbate <antolabb06@gmail.com>
2023-05-21 13:37:24 +0200

    exercise-49

--- exercise-1/src/App.js
+++ exercise-1/src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes, Link} from "react-router-dom"
 import Home from "./Home"
 import About from "./About"
 import Login from "./Login"
+import ErrorPage from "./ErrorPage"
 
 
 const App = () => {
@@ -19,6 +20,7 @@ const App = () => {
         <Route path="/" element={<Home/>}></Route>
         <Route path="/About" element={<About/>}></Route>
         <Route path="/Login" element={<Login/>}></Route>
+        <Route path="*" element={<ErrorPage/>}></Route>
       </Routes>
 
 
